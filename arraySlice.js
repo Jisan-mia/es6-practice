4c14d6188ee2b061e89abb8e1f3f209ba6147d1c
Jisan-mia <jisanmia47@gmail.com>
2021-01-25 21:28:46 +0600

    array slice method

---
+++ arraySlice.js
@@ -0,0 +1,7 @@
+// Array.prototype.slice()
+
+const numbers = [1, 2, 3, 4, 5];
+
+// slice methode will slice and return a part of an array. it takes starting index and the ending index to slice that part(it also doesn't change the main array(numbers))
+const sliceResult = numbers.slice(0, 3);
+console.log(sliceResult);
