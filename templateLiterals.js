f81e0c8c7f8040bb9cb16cb0de43d5c69d8727b4
Jisan-mia <jisanmia47@gmail.com>
2021-01-26 01:02:44 +0600

    template literals

---
+++ templateLiterals.js
@@ -0,0 +1,8 @@
+// template literalse  ``(bactic)
+
+const firstName = "Jisan";
+const lastName = "Mia";
+
+console.log(`My full name is ${firstName} ${lastName}.
+            I love my country. 
+            I enjoy playing sports.`);
