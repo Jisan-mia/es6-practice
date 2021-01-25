b049eb1071f65c055546208957ba2d146932fd13
Jisan-mia <jisanmia47@gmail.com>
2021-01-25 23:30:07 +0600

    .\funcDefaultParameter.js

---
+++ funcDefaultParameter.js
@@ -0,0 +1,13 @@
+//function default parameter
+
+//when we don't pass any parameter to a function it will return undefined
+// so we can give a default parameter value of that function
+// but if we pass parameter through calling the function the value will be the passed parameter value
+// if we pass undefined in the parameter the funciton will return the default parameter because the parameter is not difined
+// if we pass null it will return null
+
+function myFunc(x = 10) {
+	return x;
+}
+
+console.log(myFunc(undefined));
