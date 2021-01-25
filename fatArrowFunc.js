ddd21361bb7acf6df1b042b7bd7e5ed07113a13e
Jisan-mia <jisanmia47@gmail.com>
2021-01-25 19:19:14 +0600

    fatArrow func modified

--- fatArrowFunc.js
+++ fatArrowFunc.js
@@ -1,55 +1,55 @@
 //es6 fat arrow function
 // normal funciton
-// function number() {
-// 	return 10;
-// }
+function number() {
+	return 10;
+}
 
-// function number(n) {
-// 	return n;
-// }
+function number(n) {
+	return n;
+}
 
 //fat arrow function
 
-// let number = () => 10; //cannot write return in one line fat arrow func
+let number = () => 10; //cannot write return in one line fat arrow func
 
-// let number = () => console.log(10);
+let number = () => console.log(10);
 
-// let number = (a, b) => a + b;
+let number = (a, b) => a + b;
 
 // this keyword
 
-// var javaScriptObj = {
-// 	name: "JavaScript",
-// 	libraries: ["React", "Angular", "Vue"],
-// 	printLibraries: function () {
-// 		this.libraries.forEach((element) =>
-// 			console.log(`${this.name} loves ${element}`)
-// 		);
-// 	},
-// };
+var javaScriptObj = {
+	name: "JavaScript",
+	libraries: ["React", "Angular", "Vue"],
+	printLibraries: function () {
+		this.libraries.forEach((element) =>
+			console.log(`${this.name} loves ${element}`)
+		);
+	},
+};
 
-// javaScriptObj.printLibraries();
+javaScriptObj.printLibraries();
 
 // dom related
-// const searchInput = document.querySelector(".search");
-// const display = document.querySelector(".result");
-// const thanks = document.querySelector(".thanks");
+const searchInput = document.querySelector(".search");
+const display = document.querySelector(".result");
+const thanks = document.querySelector(".thanks");
 
 //normal funciton that works perfectly
-// function show() {
-// 	display.innerHTML = this.value;
-// 	var self = this;
-// 	setTimeout(() => {
-// 		console.log(this);
-// 		thanks.innerHTML = `You have typed: ${this.value}`;
-// 	}, 1000);
-// }
+function show() {
+	display.innerHTML = this.value;
+	var self = this;
+	setTimeout(() => {
+		console.log(this);
+		thanks.innerHTML = `You have typed: ${this.value}`;
+	}, 1000);
+}
 
-// const show1 = () => {
-// 	display.innerHTML = this.value; // this = window not the this of searchInput
-// };
+const show1 = () => {
+	display.innerHTML = this.value; // this = window not the this of searchInput
+};
 
-// searchInput.addEventListener("keyup", show);
+searchInput.addEventListener("keyup", show);
 
 //new keyword in fat arrow funciton
 //vanila js function is it selft is an constructor
