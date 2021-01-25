75998222d346cc13467688960a60db477af89822
Jisan-mia <jisanmia47@gmail.com>
2021-01-25 19:53:37 +0600

    truthy or falsy value modified at NaN value

--- truty_falsy_value.js
+++ truty_falsy_value.js
@@ -9,8 +9,31 @@
 
 let myVar = [];
 
-if (!myVar) {
-	console.log("I'm truthy");
-} else {
-	console.log("I'm falsy");
+// if (myVar) {
+// 	console.log("I'm truthy");
+// } else {
+// 	console.log("I'm falsy");
+// }
+
+// a function to check if the provided value is a falsy value or truthy
+function isFalsy(value) {
+	let isFalse;
+	if (value === false) {
+		isFalse = "falsy";
+	} else if (value === "") {
+		isFalse = "falsy";
+	} else if (value === 0) {
+		isFalse = "falsy";
+	} else if (value === null) {
+		isFalse = "falsy";
+	} else if (isNaN(value)) {
+		isFalse = "falsy";
+	} else if (value === undefined) {
+		isFalse = "falsy";
+	} else {
+		isFalse = "It's not a falsy value. It's truthy";
+	}
+	return isFalse;
 }
+const value = NaN;
+console.log(isFalsy(value));
