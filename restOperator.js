f4fc6b7a6c7ef860549e37d2f3f36d269b7341b0
Jisan-mia <jisanmia47@gmail.com>
2021-01-26 00:10:31 +0600

    rest operator

---
+++ restOperator.js
@@ -0,0 +1,12 @@
+//rest operator
+
+//we can get rest of the parameter passed from function calling using rest operator(...)
+// access rest parameter by (...params)
+// we can use this(...params) only one time in one function and it must be the last one of function parameter
+
+function myfunc(a, ...params) {
+	console.log(a);
+	console.log(params);
+}
+
+myfunc(4, 5, 6, 7, 10);
