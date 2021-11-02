ae48a63cc3b170ae39a636cc5781567afd63032d
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2021-11-02 22:52:17 +0600

    default parameter updated

--- funcDefaultParameter.js
+++ funcDefaultParameter.js
@@ -1,14 +1,19 @@
-//function default parameter
-
+// function default parameter value
 /*
-- when we don't pass any parameter to a function it will return undefined
-- so we can give a default parameter value of that function
-- but if we pass parameter through calling the function the value will be the passed parameter value
-- if we pass undefined in the parameter the function will return the default parameter because the parameter is not defined
-- if we pass null it will return null
+- Suppose, a function takes a parameter x and returns that x
+- now, if we forgot to pass the parameter while calling the function, it will return undefined
+- But we need the function to return something other than undefined. 
+- in that case, we can assign a default value on that parameter which will be the value of that parameter if we don't pass any arguments while calling.
+- but if we pass any arguments the parameter value will be the passed argument value
+- if we pass undefined the function will return the default parameter value because the parameter is not defined
+- if we pass null it will return null, since this time we defined the parameter value to null
 */
+
 function myFunc(x = 10) {
 	return x;
 }
 
-console.log(myFunc(undefined));
+console.log(myFunc()) // 10
+console.log(myFunc(25)) // 25
+console.log(myFunc(undefined)); // 10
+console.log(myFunc(null)) // null
