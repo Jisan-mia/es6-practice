7bf80d925e4932d0aec13c1bc4c139d20dd3904f
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2021-11-03 23:52:58 +0600

    Update restOperator.js

--- restOperator.js
+++ restOperator.js
@@ -1,6 +1,6 @@
-//rest operator
+// rest operator
 
-//we can get rest of the parameter passed from function calling using rest operator(...)
+// we can get rest of the parameter passed from function calling using rest operator(...)
 // access rest parameter by (...params)
 // we can use this(...params) only one time in one function and it must be the last one of function parameter
 
@@ -9,4 +9,4 @@ function myfunc(a, ...params) {
 	console.log(params);
 }
 
-myfunc(4, 5, 6, 7, 10);
+myfunc(4, 5, 6, 7, 10); // return: 4 [5, 6, 7, 10]
