69e0eb70decb9f0451576bb131c4db078e1c8d6b
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2021-11-04 23:26:13 +0600

    Update arrayConcat.js

--- arrayConcat.js
+++ arrayConcat.js
@@ -1,12 +1,13 @@
-//Array.prototype.concat()
+// Array.prototype.concat()
+/*
+- Array concat method combines two or more arrays at the end of the first array
+- it does not change the main array or any individual array
+*/
+const numbers1 = [1, 2];
+const numbers2 = [3, 4];
+const numbers3 = [5, 6, 7];
 
-const numbers1 = [1, 2, 3, 4, 5];
-const numbers2 = [6, 7, 8, 9, 10];
-const numbers3 = [11, 12, 13, 14, 15];
-
-//concat methods combines two or more arrays at the end of the first array
-// it does not change the main array or any individual array
 const concatTwoArr = numbers1.concat(numbers2, numbers3);
 
-console.log(numbers1);
-console.log(concatTwoArr);
+console.log(numbers1); // [1, 2]
+console.log(concatTwoArr); // [1, 2, 3, 4, 5, 6, 7]
