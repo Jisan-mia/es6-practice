af37db8a10213d1a67c809091372bc79275dd881
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2021-11-06 00:28:32 +0600

    Update arraySplice.js

--- arraySplice.js
+++ arraySplice.js
@@ -10,3 +10,17 @@ const spliceResult1 = numbers.splice(-1, 2, 6, 7, 8, 9, 10);
 
 console.log(numbers);
 console.log(spliceResult1);
+
+// Replace specific item in an array using splice method
+/*
+- when we need to replace specific items in an array, array.splice() method do a great job here. This method takes 3 parameters .splice(startIndex, deleteCount, ...items).
+	startIndex: define where to start replacing
+    deleteCount: how many items to delete
+    ...items: new items to add on top of the removed items
+*/
+
+const months = ['june', 'july', 'march', 'april', 'may'];
+
+months.splice(0, 2, 'january', 'february');
+
+console.log(months) // ['january', 'february', 'march', 'april', 'may'];
